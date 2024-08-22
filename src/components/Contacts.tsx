fed9fed528cbb1e4b51193026640c87d07687d54
Anne <anne-sophiewoite@AnneSophiesAir.fritz.box>
2024-08-22 23:33:49 +0200

    added instagram and xing icon to contacts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -6,6 +6,14 @@ function Contacts() {
         className="flex flex-col mb-10 mx-auto">
            <Title id="contact-title">Contacts</Title>
             <p className="text-md md: text-md mb-2 md:mb-3 text-center">You can reach me at <span className="font-bold">woite.anne_sophie@icloud.com</span> or by calling the following phone number <span className="font-bold"> +49 177 77 545 70</span> </p>
+        <div className="flex justify-center space-x-4">
+            <a href="https://www.instagram.com/annesophie_wt/" target="_blank" rel="noopener noreferrer">
+                <i className="bx bxl-instagram text-5xl cursor-pointer"></i>
+                </a>
+            <a href="https://www.xing.com/profile/AnneSophie_Woite068572/web_profiles?expandNeffi=true" target="_blank" rel="noopener noreferrer">
+            <i className="bx bxl-xing text-5xl cursor-pointer" ></i>
+                </a>
+            </div>
             </div> 
             
     )
