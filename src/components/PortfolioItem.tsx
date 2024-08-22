a055e33d13af200182ea505df3bcf940405f06c3
Anne <anne-sophiewoite@AnneSophiesAir.fritz.box>
2024-08-22 13:24:41 +0200

    linked pictures to site of the projects

--- src/components/PortfolioItem.tsx
+++ src/components/PortfolioItem.tsx
@@ -6,14 +6,16 @@ interface PortfolioItemProps {
     link: string;
 }
 
-function PortfolioItem({title, imgUrl, description, stack,}: PortfolioItemProps) {
+function PortfolioItem({title, imgUrl, description, stack,link}: PortfolioItemProps) {
     return (
         <div className="border-2 border-stone-900 rounded-md overflow-hidden">
+            <a href={link} target="_blank" rel="noopener noreferrer">
             <img
                 src={imgUrl}
                 alt="portfolio"
                 className="w-full h-35 md:h-48 object-cover cursor-pointer"
             />
+            </a>
             <div className="w-full p-4">
                 <h3 className="text-lg md: text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
                 <p className="text-sm text-stone-700 mb-2">
