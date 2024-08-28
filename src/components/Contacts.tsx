5d0cf49cb3d74c7af8d3e1ff228eac7cc9d66d4c
Anne <anne-sophiewoite@AnneSophiesAir.fritz.box>
2024-08-28 11:18:43 +0200

    added placeholder email and phone number

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -5,7 +5,7 @@ function Contacts() {
         <div id="contact"
         className="flex flex-col mb-10 mx-auto">
            <Title id="contact-title">Contacts</Title>
-            <p className="text-md md: text-md mb-2 md:mb-3 text-center">You can reach me at <span className="font-bold">woite.anne_sophie@icloud.com</span> or by calling the following phone number <span className="font-bold"> +49 177 77 545 70</span> </p>
+            <p className="text-md md: text-md mb-2 md:mb-3 text-center">You can reach me at <span className="font-bold">placeholder.mail@icloud.com</span> or by calling the following phone number <span className="font-bold"> place phone number here</span> </p>
         <div className="flex justify-center space-x-4">
             <a href="https://www.instagram.com/annesophie_wt/" target="_blank" rel="noopener noreferrer">
                 <i className="bx bxl-instagram text-5xl cursor-pointer"></i>
